2a8c6bf4513d3c304320e0eb12afb6b935bbf5e4
israel1x <israel.5k@hotmail.com>
2017-08-02 04:39:07 -0500

    Sprint 8 Task 354 Ver Juegos Maquetado Y estilos- lista de parametros del juego

---
+++ app/src/main/java/reactiva/reactivamovil/ParametrosJuego.java
@@ -0,0 +1,53 @@
+package reactiva.reactivamovil;
+
+/**
+ * Created by Erasmo Zurita on 2017-08-02.
+ */
+
+public class ParametrosJuego {
+
+    String detalleParametro;
+    String parametroX;
+    String parametroY;
+    String parametroZ;
+
+
+    public ParametrosJuego(String detalleParametro, String parametroX, String parametroY, String parametroZ) {
+        this.detalleParametro = detalleParametro;
+        this.parametroX = parametroX;
+        this.parametroY = parametroY;
+        this.parametroZ = parametroZ;
+    }
+
+    public String getDetalleParametro() {
+        return detalleParametro;
+    }
+
+    public void setDetalleParametro(String detalleParametro) {
+        this.detalleParametro = detalleParametro;
+    }
+
+    public String getParametroX() {
+        return parametroX;
+    }
+
+    public void setParametroX(String parametroX) {
+        this.parametroX = parametroX;
+    }
+
+    public String getParametroY() {
+        return parametroY;
+    }
+
+    public void setParametroY(String parametroY) {
+        this.parametroY = parametroY;
+    }
+
+    public String getParametroZ() {
+        return parametroZ;
+    }
+
+    public void setParametroZ(String parametroZ) {
+        this.parametroZ = parametroZ;
+    }
+}
---
+++ app/src/main/java/reactiva/reactivamovil/ParametrosJuegoAdaptador.java
@@ -0,0 +1,66 @@
+package reactiva.reactivamovil;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nancy on 2017-08-02.
+ */
+
+public class ParametrosJuegoAdaptador extends RecyclerView.Adapter<ParametrosJuegoAdaptador.ParametrosJuegoViewHolder> {
+
+    ArrayList<ParametrosJuego> parametrosDelJuegoData;
+
+    public ParametrosJuegoAdaptador(ArrayList<ParametrosJuego> parametrosDelJuegoData) {
+        this.parametrosDelJuegoData = parametrosDelJuegoData;
+    }
+
+    @Override
+    public ParametrosJuegoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_parametros_juego,parent,false);
+
+        return new ParametrosJuegoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ParametrosJuegoViewHolder parametrosJuegoViewHolder, int position) {
+
+        ParametrosJuego parametrosJuego = parametrosDelJuegoData.get(position);
+        parametrosJuegoViewHolder.tvDetalleParametroJuego.setText(parametrosJuego.getDetalleParametro());
+        parametrosJuegoViewHolder.tvParametroX.setText(parametrosJuego.getParametroX());
+        parametrosJuegoViewHolder.tvParametroY.setText(parametrosJuego.getParametroY());
+        parametrosJuegoViewHolder.tvParametroZ.setText(parametrosJuego.getParametroZ());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return parametrosDelJuegoData.size();
+    }
+
+
+    public static class ParametrosJuegoViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvDetalleParametroJuego;
+        private TextView tvParametroX;
+        private TextView tvParametroY;
+        private TextView tvParametroZ;
+
+        public ParametrosJuegoViewHolder(View itemView) {
+            super(itemView);
+
+            tvDetalleParametroJuego = (TextView) itemView.findViewById(R.id.tvDetalleParamJ);
+            tvParametroX = (TextView) itemView.findViewById(R.id.tvParamX);
+            tvParametroY = (TextView) itemView.findViewById(R.id.tvParamY);
+            tvParametroZ = (TextView) itemView.findViewById(R.id.tvParamZ);
+
+        }
+    }
+
+
+}
